1e79af562663c44377f2960030fd384d9a583912
Damar Albaribin <damaralbaribin@gmail.com>
2022-05-18 01:34:07 +0700

    fix : any

--- modules/mailer.ts
+++ modules/mailer.ts
@@ -1,29 +1,26 @@
-import axios from "axios";
-import { getConfig } from './config';
+import axios from 'axios'
+import { getConfig } from './config'
 
 const mailClient = axios.create({
-    baseURL: getConfig("MAIL_BASE_API_URL"),
-    headers: {
-        "X-Server-API-Key": getConfig("MAIL_API_KEY")
-    }
+  baseURL: 'https://postal.damaral.my.id',
+  headers: {
+    'X-Server-API-Key': getConfig('MAIL_API_KEY'),
+  },
 })
 
-const SEND_MAIL_ROUTE = "/api/v1/send/message"
-
+const SEND_MAIL_ROUTE = '/api/v1/send/message'
 
 interface MailBody {
-    subject: string;
-    plain_body?: string;
-    html_body?: string;
-    to: string[];
+  subject: string
+  plain_body?: string
+  html_body?: string
+  to: string[]
 }
 
 export const sendMail = async (data: MailBody) => {
-
-    return await mailClient.post(SEND_MAIL_ROUTE, {
-        sender: "noreply@postal.damaral.my.id",
-        from: "noreply@postal.damaral.my.id",
-        ...data
-    })
-
+  return await mailClient.post(SEND_MAIL_ROUTE, {
+    sender: 'noreply@postal.damaral.my.id',
+    from: 'noreply@postal.damaral.my.id',
+    ...data,
+  })
 }
